c7690c0f7d24228bbdad9203e1c1599be313851b
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2024-08-21 07:18:32 +0700

    update

--- resources/views/layouts/client/header.blade.php
+++ resources/views/layouts/client/header.blade.php
@@ -7,11 +7,16 @@ $package = Package::where('type', 'S')->first();
 
 <header class="header px-3 mb-3 fixed-top border-bottom">
     <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-between">
-            <div class="d-flex align-items-center justify-content-center">
-                <img class="img-fluid border rounded-circle p-1" width="60" src="{{ asset('assets/img/logo.png') }}"
-                    alt="Logo">
-                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 mx-3 justify-content-center mb-md-0">
+        <nav class="navbar navbar-expand-lg py-0">
+            <a class="navbar-brand" href="#">
+                <img class="img img-fluid me-3" width="60" src="{{ asset('assets/img/logo.png') }}" alt="Logo">
+            </a>
+            <button class="navbar-toggler aktif" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon""></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li><a href="{{ route('beranda') }}"
                             class="nav-link py-3 px-3 fw-bold fs-5 @yield('textHome')">{{ __('Home') }}</a>
                     </li>
@@ -25,35 +30,35 @@ $package = Package::where('type', 'S')->first();
                             class="nav-link py-3 px-3 fw-bold fs-5 @yield('textBooking')">{{ __('My Booking') }}</a>
                     </li>
                 </ul>
+                <ul class="navbar-nav ms-auto">
+                    @if (Auth::check())
+                        <li class="nav-item dropdown @yield('textProfile')">
+                            <a class="nav-link dropdown-toggle d-flex align-items-center" href="#"
+                                id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"
+                                style="color: #0093E9">
+                                {{ Str::limit(auth()->user()->name, 15) }} <i
+                                    class="fas fa-user-circle ms-2 fa-2x"></i>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <li>
+                                    <a href="{{ route('profile.edit') }}" class="dropdown-item">Profile</a>
+                                </li>
+                                <li>
+                                    <form action="{{ route('logout') }}" method="POST">
+                                        @csrf
+                                        <button type="submit" class="dropdown-item">Logout</button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a href="{{ route('login') }}" class="btn btn-primary aktif ms-3">Login</a>
+                        </li>
+                    @endif
+                </ul>
             </div>
-            <div class="d-none d-lg-block">
-                @if (Auth::check())
-                    <div class="dropdown">
-                        <button class="btn dropdown-toggle d-flex align-items-center" type="button"
-                            id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false"
-                            style="color: #0093E9">
-                            {{ Auth::user()->name }} <i class="fas fa-user-circle ms-2 fa-2x"></i>
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
-                            <li>
-                                <a href="{{ route('profile.edit') }}" class="dropdown-item">Profile</a>
-                            </li>
-                            <li>
-                                <form action="{{ route('logout') }}" method="POST">
-                                    @csrf
-                                    <button type="submit" class="dropdown-item">Logout</button>
-                                </form>
-                            </li>
-                        </ul>
-                    </div>
-                @else
-                    <div class="d-flex justify-content-center align-items-center">
-                        <a href="{{ route('login') }}" class="btn aktif btn-outline-light ms-3"><i
-                                class="fas fa-sign-in-alt"></i> Login</a>
-                    </div>
-                @endif
-            </div>
-        </div>
+        </nav>
     </div>
 </header>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
